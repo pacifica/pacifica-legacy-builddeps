a8e5dd4e075a0a58cebec59bfae170a1cda5c78b
David Brown <david.brown@pnnl.gov>
2012-11-29 10:51:36 -0800

    this should remove all versioned files

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-JQUERY=1.8.2
+JQUERY=1.8.3
 JQUERYUI=1.9.2
-JQUERYFORMWIZARD=3.0.5
+JQUERYFORMWIZARD=3.0.7
 QUNIT=1.10.0
 
-VERSION=013
+VERSION=014
 
 if git tag -l | grep -q $VERSION
 then
@@ -15,11 +15,11 @@ fi
 
 mkdir -p code
 pushd code
-wget -O jquery-$JQUERY.min.js http://code.jquery.com/jquery-$JQUERY.min.js
-wget -O jquery-$JQUERY.js http://code.jquery.com/jquery-$JQUERY.js
+wget -O jquery.min.js http://code.jquery.com/jquery-$JQUERY.min.js
+wget -O jquery.js http://code.jquery.com/jquery-$JQUERY.js
 ../form-download.py -f jquery-ui-$JQUERYUI.custom.zip
-wget -O qunit-$QUNIT.js http://code.jquery.com/qunit/qunit-$QUNIT.js
-wget -O qunit-$QUNIT.css http://code.jquery.com/qunit/qunit-$QUNIT.css
+wget -O qunit.js http://code.jquery.com/qunit/qunit-$QUNIT.js
+wget -O qunit.css http://code.jquery.com/qunit/qunit-$QUNIT.css
 rm -rf r.js
 git clone https://github.com/jrburke/r.js.git
 rm -rf qTip2
@@ -49,9 +49,9 @@ mv */dist/* ..
 popd
 rm -rf html5shiv/
 
-mkdir jquery-ui-$JQUERYUI; pushd jquery-ui-$JQUERYUI/; unzip -o ../jquery-ui-$JQUERYUI.custom.zip; popd
+unzip -o jquery-ui-$JQUERYUI.custom.zip; mv jquery-ui-$JQUERYUI.custom jquery-ui
 rm -f jquery-ui-$JQUERYUI.custom.zip
-mkdir jquery-formwizard-$JQUERYFORMWIZARD; pushd jquery-formwizard-$JQUERYFORMWIZARD; unzip -o ../jquery.formwizard-$JQUERYFORMWIZARD.zip; popd
+mkdir jquery.formwizard; pushd jquery.formwizard ;unzip -o ../jquery.formwizard-$JQUERYFORMWIZARD.zip; popd
 rm -f jquery.formwizard-$JQUERYFORMWIZARD.zip
 
 popd
