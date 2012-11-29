c91c96894132f580b9a624d263050990134c5a89
David Brown <david.brown@pnnl.gov>
2012-11-29 08:53:53 -0800

    add removal of the recursive symlink

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ eval $(grep ^VERSION update.sh)
 ln -s . myemsl-builddeps-$VERSION
 tar \
   --exclude=myemsl-builddeps-$VERSION/myemsl-builddeps-$VERSION.tar.gz \
+  --exclude=myemsl-builddeps-$VERSION/myemsl-builddeps-$VERSION \
   --exclude=myemsl-builddeps-$VERSION/.git \
   -czf myemsl-builddeps-$VERSION.tar.gz \
   myemsl-builddeps-$VERSION/*
