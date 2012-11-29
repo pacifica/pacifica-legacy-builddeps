5ae0142b4671385019d52291f3e310d05b4c6a51
David Brown <david.brown@pnnl.gov>
2012-11-29 08:53:04 -0800

    add the build tarball script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+eval $(grep ^VERSION update.sh)
+
+ln -s . myemsl-builddeps-$VERSION
+tar \
+  --exclude=myemsl-builddeps-$VERSION/myemsl-builddeps-$VERSION.tar.gz \
+  --exclude=myemsl-builddeps-$VERSION/.git \
+  -czf myemsl-builddeps-$VERSION.tar.gz \
+  myemsl-builddeps-$VERSION/*
+
+rm myemsl-builddeps-$VERSION
