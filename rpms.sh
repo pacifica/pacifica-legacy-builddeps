4780e97e14686a08fd96b9352c3000621dca3d85
David Brown <david.brown@pnnl.gov>
2013-05-08 11:13:59 -0700

    need to fix the script

--- rpms.sh
+++ rpms.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=$( grep ^VERSION update.sh)
+eval $( grep ^VERSION update.sh)
 
 ./build.sh
 rpmbuild -ta pacifica-builddeps-$VERSION.tar.gz --define '_rpmdir '`pwd`'/packages/bin' --define '_srcrpmdir '`pwd`'/packages/src' --define "LOCALREVISION `./svnver.sh .`"
